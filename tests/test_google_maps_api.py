9bc5abe6a52bafd62c9981bdf07b5da19985ee25
Maiia Orlovsky <orlovsky-maya@mail.ru>
2023-10-09 08:02:20 +0400

    Added files: - deleting_location_body.json - update_location_body.json Updated files: - test_google_maps_api - api - conftest - pytest.ini

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,7 +1,25 @@
-from requests import Response
 from utils.api import GoogleMapsApi
 
 
 def test_create_new_place():
     print('Method POST')
-    post_result: Response = GoogleMapsApi.create_location()
+    post_result = GoogleMapsApi.create_location()
+
+    info_json = post_result.json()
+    place_id = info_json.get('place_id')
+
+    print('Method GET POST')
+    get_result = GoogleMapsApi.get_location(place_id)
+
+    print('Method PUT')
+    put_result = GoogleMapsApi.put_location(place_id)
+
+    print('Method GET PUT')
+    get_result = GoogleMapsApi.get_location(place_id)
+
+    print('Method DELETE')
+    delete_result = GoogleMapsApi.delete_location(place_id)
+
+    print('Method GET DELETE')
+    get_result = GoogleMapsApi.get_location(place_id)
+
--- utils/api.py
+++ utils/api.py
@@ -1,10 +1,12 @@
 import json
+from pathlib import Path
 from utils.http_methods import HttpMethods
 
 
 class GoogleMapsApi:
     base_url = 'https://rahulshettyacademy.com'
     key = '?key=qaclick123'
+    d = Path(__file__).parent.absolute()
 
     @classmethod
     def create_location(cls):
@@ -16,7 +18,7 @@ class GoogleMapsApi:
 
         # Open json file
 
-        with open('./tests/new_location_body.json', 'r', encoding='utf-8') as body_json:
+        with open(f'{cls.d}/new_location_body.json', 'r', encoding='utf-8') as body_json:
             body_json = json.load(body_json)
 
         # POST request
@@ -24,3 +26,58 @@ class GoogleMapsApi:
         post_result = HttpMethods.post(post_url, body_json)
         print(post_result.text)
         return post_result
+
+    @classmethod
+    def get_location(cls, place_id):
+        # Getting get url
+
+        get_resource = '/maps/api/place/get/json'
+        get_url = cls.base_url + get_resource + cls.key + '&place_id=' + place_id
+        print(get_url)
+
+        # GET request
+
+        get_result = HttpMethods.get(get_url)
+        print(get_result.text)
+        return get_result
+
+    @classmethod
+    def put_location(cls, place_id):
+        # Getting put url
+
+        put_resource = '/maps/api/place/update/json'
+        put_url = cls.base_url + put_resource + cls.key
+        print(put_url)
+
+        # Open json file
+
+        with open(f'{cls.d}/update_location_body.json', 'r', encoding='utf-8') as body_json:
+            body_json = json.load(body_json)
+            body_json['place_id'] = place_id
+
+        # PUT Request
+
+        put_result = HttpMethods.put(put_url, body_json)
+        print(put_result.text)
+        return put_result
+
+    @classmethod
+    def delete_location(cls, place_id):
+        # Getting delete url
+
+        delete_resource = '/maps/api/place/delete/json'
+        delete_url = cls.base_url + delete_resource + cls.key
+        print(delete_url)
+
+        # Open json file
+
+        with open(f'{cls.d}/update_location_body.json', 'r', encoding='utf-8') as body_json:
+            body_json = json.load(body_json)
+            body_json['place_id'] = place_id
+
+        # DELETE Request
+
+        delete_result = HttpMethods.delete(delete_url, body_json)
+        print(delete_result.text)
+        return delete_result
+
